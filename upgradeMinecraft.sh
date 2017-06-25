e6c70a5cc3ca08a1326743fe4207fcf917f17b9a
BuildTools <unconfigured@null.spigotmc.org>
2017-06-25 05:40:43 +0000

    More updates

--- upgradeMinecraft.sh
+++ upgradeMinecraft.sh
@@ -2,10 +2,13 @@
 
 # Run this script with sudo, e.g. 'sudo upgradeMinecraft.sh"
 #
-# Assumes you are Internet-connected, which might  require temporarily disabling
-# the offline static IP address using 'sudo nano /etc/network/interfaces' and commenting out
+# Assumes you are Internet-connected, which might require temporarily disabling
+# the static IP address to the wifi adapter using 'sudo nano /etc/network/interfaces' and commenting out
 # the static server configuration for eth0, and enabling automatic DHCP instead.
 
+if [! -d ./MineCraft]; then
+    mkdir ./MineCraft
+fi
 cd ./MineCraft
 
 # Update system
@@ -13,15 +16,17 @@ echo
 echo Upgrading all packages on system
 echo
 apt-get update
+apt-get safe-upgrade
 
 if [-f BuildTools.jar]; then
     rm BuildTools.jar
 fi
 
 # 'Spigot' optimized MineCraft server - https://spigotmc.org/wiki/buildtools
+SPIGOT_VERSION=1.12
 echo
-echo Downloading latest Spigot server buildtools and building 1.12
+echo Downloading latest Spigot server buildtools and building $SPIGOT_VERSION
 echo
-wget https:// hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 git config --global --unset core.autocrlf
-java -jar BuildTools.jar --rev 1.12
+java -jar BuildTools.jar --rev $SPIGOT_VERSION
